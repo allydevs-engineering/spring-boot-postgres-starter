58080d896d7f9c2ad78ff29508a27d2e7f324d83
Admin AllyDevs <admin@allydevs.com>
2026-08-30 14:41:48 +0530

    chore: add code quality tooling

--- src/main/java/com/allydevs/persistence/SpringBootPostgresStarterApplication.java
+++ src/main/java/com/allydevs/persistence/SpringBootPostgresStarterApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootPostgresStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPostgresStarterApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootPostgresStarterApplication.class, args);
+  }
 }
--- src/test/java/com/allydevs/persistence/SpringBootPostgresStarterApplicationTests.java
+++ src/test/java/com/allydevs/persistence/SpringBootPostgresStarterApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootPostgresStarterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
