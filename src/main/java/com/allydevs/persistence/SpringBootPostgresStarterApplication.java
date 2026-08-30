097bd3715632031c98e23f7131b83128c73792d1
Admin AllyDevs <admin@allydevs.com>
2026-08-30 14:39:14 +0530

    chore: initialize postgres persistence starter

---
+++ src/main/java/com/allydevs/persistence/SpringBootPostgresStarterApplication.java
@@ -0,0 +1,13 @@
+package com.allydevs.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresStarterApplication.class, args);
+	}
+
+}
