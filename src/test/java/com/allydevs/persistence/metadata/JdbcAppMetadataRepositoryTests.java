f20dba43cbec45f9fbe9c7203d0665f585c8f024
Admin AllyDevs <admin@allydevs.com>
2026-08-30 20:12:01 +0530

    fix: integration test configuration

--- src/test/java/com/allydevs/persistence/metadata/JdbcAppMetadataRepositoryTests.java
+++ src/test/java/com/allydevs/persistence/metadata/JdbcAppMetadataRepositoryTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
