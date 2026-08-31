47a8aecbca34327e9355ec172f477d4a6b0d8983
Admin AllyDevs <admin@allydevs.com>
2026-08-31 11:52:32 +0530

    fix: separation of no db tests and integration tests

--- src/test/java/com/allydevs/persistence/SpringBootPostgresStarterApplicationTests.java
+++ src/test/java/com/allydevs/persistence/SpringBootPostgresStarterApplicationTests.java
@@ -2,8 +2,10 @@ package com.allydevs.persistence;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class SpringBootPostgresStarterApplicationTests {
 
   @Test
--- src/test/java/com/allydevs/persistence/config/DatabasePropertiesTests.java
+++ src/test/java/com/allydevs/persistence/config/DatabasePropertiesTests.java
@@ -3,19 +3,32 @@ package com.allydevs.persistence.config;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
-@SpringBootTest
 class DatabasePropertiesTests {
 
-  @Autowired private DatabaseProperties databaseProperties;
+  private final ApplicationContextRunner contextRunner =
+      new ApplicationContextRunner()
+          .withUserConfiguration(TestConfiguration.class)
+          .withPropertyValues(
+              "spring.datasource.url=jdbc:postgresql://localhost:5432/ad_sb_pg_starter",
+              "spring.datasource.username=postgres",
+              "spring.datasource.password=postgres");
 
   @Test
   void loadsDatabaseConfiguration() {
-    assertThat(databaseProperties.url())
-        .isEqualTo("jdbc:postgresql://localhost:5432/ad_sb_pg_starter");
+    contextRunner.run(
+        context -> {
+          DatabaseProperties databaseProperties = context.getBean(DatabaseProperties.class);
 
-    assertThat(databaseProperties.username()).isEqualTo("postgres");
+          assertThat(databaseProperties.url())
+              .isEqualTo("jdbc:postgresql://localhost:5432/ad_sb_pg_starter");
+
+          assertThat(databaseProperties.username()).isEqualTo("postgres");
+        });
   }
+
+  @EnableConfigurationProperties(DatabaseProperties.class)
+  static class TestConfiguration {}
 }
